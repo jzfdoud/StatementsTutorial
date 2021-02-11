0794a8f355160c1027752aeb21a9a23b0080ddc4
Jada <jada.fagan@gmail.com>
2021-02-11 16:05:59 -0500

    Add project files.

---
+++ StatementsTutorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StatementsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            var aNbr = Console.ReadLine();
+            var Nbr = Convert.ToInt32(aNbr);
+                if(Nbr % 2 ==0)
+            { Console.WriteLine($"{aNbr} is even!"); }
+                else
+            { Console.WriteLine($"{aNbr} is odd :("); }
+
+
+
+            var ans = 0;
+            for (var nbr = 0; nbr <= 50; nbr++)
+            {
+                if (nbr % 5 == 0 || nbr % 7 == 0)
+                    ans += nbr;
+            }
+            Console.WriteLine($"This is the sum of numbers 1 to 50 divisable by 5 or 7 : {ans}");
+        }
+    }
+}
